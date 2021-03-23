14e3f8df6c5f82f12b8b77ca8141da3a35415d82
Artem Sivyankov <morfiy0393@gmail.com>
2021-03-24 01:11:15 +0300

    refactor: adjust HTML, CSS,JS-formatting according to rule 3.3 in html-and-css.md

--- app.js
+++ app.js
@@ -9,8 +9,8 @@
 
 var taskInput = document.getElementById('new-task-input'); //Add a new task.
 var addButton = document.getElementsByTagName('button')[0]; //first button
-var incompleteTaskHolder = document.getElementById('incomplete-tasks'); //ul of #incompleteTasks
-var completedTasksHolder = document.getElementById('completed-tasks'); //completed-tasks
+var incompleteTaskHolder = document.getElementById('incomplete'); //ul of #incompleteTasks
+var completedTasksHolder = document.getElementById('completed'); //completed-tasks
 
 //New task list item
 var createNewTaskElement = function (taskString) {
